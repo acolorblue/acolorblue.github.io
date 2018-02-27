d760a857c81ab70ea51f4488c6659e02857403dc
ACOLORBLUE <blueacolor@gmail.com>
2018-02-26 19:19:15 -0800

    Update iMessage.js

--- iMessage.js
+++ iMessage.js
@@ -1,1 +1,15 @@
+// SIGN OUT
+$('button.sign-out').click(function() {
+  this_button = $(this).closest('button.sign-out');
+  iosPromptLibrary();
+});
 
+
+
+
+// DELETE
+$('button.delete').click(function() {
+  this_button = $(this).closest('button.delete');
+  comment_to_delete = $(this).closest('.single-message-container');
+  iosPromptLibrary();
+});
