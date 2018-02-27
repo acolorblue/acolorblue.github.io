fc3e73812e9523b2156d8f91ca70834890d2b86b
ACOLORBLUE <blueacolor@gmail.com>
2018-02-27 13:05:20 -0800

    Update iMessage.js

--- iMessage.js
+++ iMessage.js
@@ -1,9 +1,26 @@
+// USER HANDLE DETERMINE
+function userHandleDetermine(targetElement) {
+  user_handle = $(targetElement).closest('.single-message-container, .social-media-embed').find('.user-handle').first().text();
+}
+
+
+
+
 // SIGN OUT
 $('button.sign-out').click(function() {
   this_button = $(this).closest('button.sign-out');
   iosPromptLibrary();
 });
 
+
+
+
+// MUTE USER
+$('.mute-user').click(function() {
+  this_button = $(this).closest('.mute-user');
+  userHandleDetermine(this_button);
+  iosPromptLibrary();
+});
 
 
 
