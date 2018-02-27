a52d74939942335efefc7a41902c9494e57bb2c3
ACOLORBLUE <blueacolor@gmail.com>
2018-02-26 18:52:03 -0800

    Update global.js

--- global.js
+++ global.js
@@ -1,8 +1,13 @@
+// GLOBAL VARIABLES
+var this_button,
+    comment_to_delete,
+    social_media_provider,
+    user_handle;
+
+
+
+
 // ADD CLASSES 'scale-down hide' TO ELEMENT 
 function addScaleDownAndHide(targetElement) {
   $(targetElement).addClass('scale-down hide');
 }
-
-
-
-
