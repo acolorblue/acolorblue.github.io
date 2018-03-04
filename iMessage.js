99b4884d9fe3d35312315d8ce1e1fc18ae405c85
ACOLORBLUE <blueacolor@gmail.com>
2018-03-03 19:38:33 -0800

    Update iMessage.js

--- iMessage.js
+++ iMessage.js
@@ -53,3 +53,11 @@ $('button.delete').click(function() {
   comment_to_delete = $(this).closest('.single-message-container');
   iosPromptLibrary();
 });
+
+
+
+
+// WINDOW ON LOAD
+window.onload = function() {
+  sameUser();
+}
