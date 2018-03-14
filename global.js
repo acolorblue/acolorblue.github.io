2b603ce6f38abfc726e8644097fffc57026aa59f
ACOLORBLUE <blueacolor@gmail.com>
2018-03-13 18:03:19 -0700

    Update global.js

--- global.js
+++ global.js
@@ -1,3 +1,12 @@
+// GLOBAL VARIABLES
+var ios = navigator.userAgent.match(/iPhone/i) ||
+          navigator.userAgent.match(/iPad/i) ||
+          navigator.userAgent.match(/iPod/i),
+    android = navigator.userAgent.match(/Android/i);
+
+
+
+
 // ADD CLASSES 'scale-down hide' TO ELEMENT 
 function addScaleDownAndHide(targetElement) {
   $(targetElement).addClass('scale-down hide');
