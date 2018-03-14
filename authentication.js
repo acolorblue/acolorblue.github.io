b4a63170bfee186a8d46f4c76cad5bc8b6b28639
ACOLORBLUE <blueacolor@gmail.com>
2018-03-13 19:07:10 -0700

    Update authentication.js

--- authentication.js
+++ authentication.js
@@ -1,36 +1,14 @@
-// // USER PROVIDER
-// function userProvider(targetElement) {
-//   if (targetElement.parents('.twitter, .twitter-embed').length) {
-//     social_media_provider = "Twitter";
-//   }
-  
-//   if (targetElement.parents('.instagram, .instagram-embed').length) {
-//     social_media_provider = "Instagram";
-//   }
-  
-//   if (targetElement.parents('.tumblr, .tumblr-embed').length) {
-//     social_media_provider = "Tumblr";
-//   }
-  
-//   if (social_media_provider == null) {
-//     social_media_provider = "Social Media";
-//   }
-// }
-
-
-
-
 // USER PROVIDER
 function userProvider(targetElement) {
-  if (targetElement.parents('.twitter, .twitter-embed')) {
+  if (targetElement.parents('.twitter, .twitter-embed').length) {
     social_media_provider = "Twitter";
   }
   
-  if (targetElement.parents('.instagram, .instagram-embed')) {
+  if (targetElement.parents('.instagram, .instagram-embed').length) {
     social_media_provider = "Instagram";
   }
   
-  if (targetElement.parents('.tumblr, .tumblr-embed')) {
+  if (targetElement.parents('.tumblr, .tumblr-embed').length) {
     social_media_provider = "Tumblr";
   }
   
