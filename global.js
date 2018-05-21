b3833285dd6ad8ce2df4d0fb4c00648b4c7911a2
ACOLORBLUE <blueacolor@gmail.com>
2018-05-20 20:31:02 -0700

    Update global.js

--- global.js
+++ global.js
@@ -3,6 +3,7 @@ var ios = navigator.userAgent.match(/iPhone/i) ||
           navigator.userAgent.match(/iPad/i) ||
           navigator.userAgent.match(/iPod/i),
     android = navigator.userAgent.match(/Android/i),
+    mobile = ios || android,
     twitterInAppBrowser = navigator.userAgent.includes("Twitter"),
     instagramInAppBrowser = navigator.userAgent.includes("Instagram"),
     safariMobile = navigator.userAgent.includes("Safari") && navigator.userAgent.includes("iPhone"),
